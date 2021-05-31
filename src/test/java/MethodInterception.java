3cac5d626921b03605e4fb6e01ea652f5c5a4e30
DamirGlobal <74592328+DamirGlobal@users.noreply.github.com>
2021-05-31 16:32:48 +0500

    First commit for first sample of test

---
+++ src/test/java/MethodInterception.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+@interface Selector {
+
+    String xpath();
+}
+
+interface MainPage {
+
+    @Selector(xpath = ".//*[@test-attr='input_search']")
+    String textInputSearch();
+
+    @Selector(xpath = ".//*[@test-attr='button_search']")
+    String buttonSearch();
+}
+
+public class MethodInterception {
+
+    @Test
+    public void annotationValue() {
+        MainPage mainPage = createPage(MainPage.class);
+        Assertions.assertNotNull(mainPage);
+        Assertions.assertEquals(mainPage.buttonSearch(), ".//*[@test-attr='button_search']");
+        Assertions.assertEquals(mainPage.textInputSearch(), ".//*[@test-attr='input_search']");
+    }
+
+    private MainPage createPage(Class clazz) {
+    return (MainPage) Proxy.newProxyInstance(
+            ClassLoader.getSystemClassLoader(),
+            new Class<?>[] { clazz },
+            (proxy, method, args) -> {
+                String name = method.getName();
+                if (name.contains("textInputSearch") || (name.contains("buttonSearch"))) {
+                    return  method.getAnnotation(Selector.class).xpath();
+                }
+                return null;
+            });
+    }
+}
